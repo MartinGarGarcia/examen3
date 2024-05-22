98777e93beb8aa391fb00e754671919264b756df
persona <persona@10.0.2.15>
2024-05-22 17:46:54 +0200

    Primer Commit

---
+++ src/examen3/Examen3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen3;
+
+/**
+ *
+ * @author persona
+ */
+public class Examen3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
