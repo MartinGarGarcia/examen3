299d35b3b45efe9d9fbf60998c5cb96b9eff3531
persona <persona@10.0.2.15>
2024-05-22 18:36:53 +0200

    Añadido javadoc

--- src/examen3refactorizado/CCuenta.java
+++ src/examen3refactorizado/CCuenta.java
@@ -1,30 +1,86 @@
 package examen3refactorizado;
 
 public class CCuenta {
+
+    /**
+     * @return the dSaldo
+     */
+    public double getdSaldo() {
+        return dSaldo;
+    }
+
+    /**
+     * @param dSaldo the dSaldo to set
+     */
+    public void setdSaldo(double dSaldo) {
+        this.dSaldo = dSaldo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     // Propiedades de la Clase Cuenta
-    public double dSaldo;
-    public String nombre;
+    /**
+     * Los atributos de esta clase son el saldo y el nombre
+     * @author Martin Garcia
+     * @since 22/05/2024
+     */
+    private double dSaldo;
+    private String nombre;
 
     public static void main(String[] args) {
 
-        CCuenta miCuenta = new CCuenta();
+        CCuenta cuenta1 = new CCuenta();
+        operativa_cuenta(cuenta1, 100);
 
-        System.out.println("Saldo Incial: " + miCuenta.dSaldo + " euros");
-
-        miCuenta.ingresar(-100);
-        System.out.println("Saldo tras ingreso: " + miCuenta.dSaldo + " euros");
-        miCuenta.ingresar(0);
-        System.out.println("Saldo tras ingreso: " + miCuenta.dSaldo + " euros");
-        miCuenta.ingresar(100);
-        System.out.println("Saldo tras ingreso: " + miCuenta.dSaldo + " euros");
-        miCuenta.ingresar(3000);
-        System.out.println("Saldo tras ingreso: " + miCuenta.dSaldo + " euros");
-        miCuenta.ingresar(3001);
-        System.out.println("Saldo tras ingreso: " + miCuenta.dSaldo + " euros");
+       
       
+    }
+    /**
+     * 
+     * @param cuenta es la cuenta sobre la que se realiza la operativa
+     * @param cantidad es la cantidad a ingresar o retirar
+     * @author Martin Garcia
+     * @since 22/05/2024
+     */
+    public static void operativa_cuenta(CCuenta cuenta, float cantidad) {
+        System.out.println("Saldo Incial: " + cuenta.getdSaldo() + " euros");
+        
+        cuenta.ingresar(-100);
+        System.out.println("Saldo tras ingreso: " + cuenta.getdSaldo() + " euros");
+        cuenta.ingresar(0);
+        System.out.println("Saldo tras ingreso: " + cuenta.getdSaldo() + " euros");
+        cuenta.ingresar(100);
+        System.out.println("Saldo tras ingreso: " + cuenta.getdSaldo() + " euros");
+        cuenta.ingresar(3000);
+        System.out.println("Saldo tras ingreso: " + cuenta.getdSaldo() + " euros");
+        cuenta.ingresar(3001);
+        System.out.println("Saldo tras ingreso: " + cuenta.getdSaldo() + " euros");
+        cuenta.ingresar(cantidad);
+        System.out.println("Saldo tras ingreso: " + cuenta.getdSaldo() + " euros");
+        cuenta.retirar(cantidad);
+        System.out.println("Saldo tras retirada: " + cuenta.getdSaldo() + " euros");
+
     }
 
     /* Metodo para ingresar cantidades en la cuenta. Modifica el saldo. */
+    /**
+     * 
+     * @param cantidad es la cantidad que se ingresa
+     * @return retorna el código de error
+     * @author Martin Garcia
+     * @since 22/05/2024
+     */
     public int ingresar(double cantidad) {
         int iCodErr;
         /* Inicialmente el código no evaluaba correctamente la expresión porque la estaba negando !() por lo tanto
@@ -36,10 +92,10 @@ public class CCuenta {
         } else if (cantidad == 3000) {
             System.out.println(
                     "Le recordamos que estamos obligados a notificar a Hacienda dicho ingreso (superior o igual a 3000€)");
-            dSaldo = dSaldo + cantidad;
+            setdSaldo(getdSaldo() + cantidad);
             iCodErr = 0;
         } else {
-            dSaldo = dSaldo + cantidad;
+            setdSaldo(getdSaldo() + cantidad);
             iCodErr = 0;
         }
 
@@ -51,14 +107,20 @@ public class CCuenta {
     /*
      * Metodo para retirar cantidades en la cuenta. Modifica el saldo.
      */
+    /**
+     * 
+     * @param cantidad es la cantidad a retirar
+     * @author Martin Garcia
+     * @since 22/05/2024
+     */
     public void retirar(double cantidad) {
         if (cantidad <= 0) {
             System.out.println("No se puede retirar una cantidad negativa o de 0"); 
-        } else if (dSaldo < cantidad) {
+        } else if (getdSaldo() < cantidad) {
             System.out.println("No  hay suficiente saldo");
         } else {
-            dSaldo = dSaldo - cantidad;
-            System.out.println("Le quedan " + dSaldo + " en cuenta");
+            setdSaldo(getdSaldo() - cantidad);
+            System.out.println("Le quedan " + getdSaldo() + " en cuenta");
         }
     }
 }
